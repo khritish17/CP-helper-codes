5e77e9fb51319535f857623974cb5a254f265986
Khritish Kumar Behera <khritish.official@gmail.com>
2024-05-17 14:27:39 +0530

    DISJOINT SET UNION-FIND CODES

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,41 +1,20 @@
-class DisjointSet:
-    def __init__(self, n) -> None:
-        self.parent = [i for i in range(0, (n + 1))]
-        print(self.parent)
-        self.rank = [0] * (n + 1)
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
     
     def find(self, u):
-        parent = self.parent[u]
-        while parent != u:
-            parent = self.parent[u] 
-            u = parent
-        return parent
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])  
+        return self.parent[u]
     
     def union(self, u, v):
-        # check if both parents are same or not
         parent_u, parent_v = self.find(u), self.find(v)
-        if parent_u == parent_v:
-            return 
+        rank_u, rank_v = self.rank[u], self.rank[v]
+        if rank_u >= rank_v:
+            self.parent[parent_v] = parent_u
+        elif rank_u < rank_v:
+            self.parent[parent_u] = parent_v
         else:
-            rank_u, rank_v = self.rank[parent_u], self.rank[parent_v]
-            if rank_u > rank_v:
-                self.parent[parent_v] = parent_u
-            elif rank_u < rank_v:
-                self.parent[parent_u] = parent_v
-            else:
-                self.parent[parent_v] = parent_u
-                self.rank[parent_u] += 1
-    def same_set(self, u, v):
-        return self.find(u) == self.find(v)     
-    
-# ds = DisjointSet(6)
-
-# ds.union(0, 1)
-# ds.union(1, 2)
-# ds.union(3, 4)
-
-# print(ds.same_set(0, 1))
-# print(ds.same_set(1, 2))
-# print(ds.same_set(2, 3))
-# print(ds.same_set(3, 4))
-# print(ds.same_set(4, 5))
+            self.parent[parent_v] = parent_u
+            self.rank[parent_u] += 1
